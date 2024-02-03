19d7fe194b9274d5be0da67537cc6958897650a8
ali amrol <aliamrol@aut.ac.ir>
2024-02-03 15:26:22 +0330

    add freezed

--- lib/ReviewBloc/review_state.dart
+++ lib/ReviewBloc/review_state.dart
@@ -1,28 +1,33 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
 import '../Entities/lesson_entity.dart';
 
-abstract class ReviewState {
-  late LessonEntity lessonEntity;
+@freezed
+class ReviewState with _$ReviewState {
+  const factory ReviewState.Loading() = _ReviewLoadingState;
+
+  const factory ReviewState.Error() = _ReviewErrorState;
+
+  const factory ReviewState.Next() = _ReviewNextState;
+
+  const factory ReviewState.Previous() = _ReviewPreviousState;
+
+  const factory ReviewState.Complete() = _ReviewCompleteState;
 }
 
-class ReviewLoadingState extends ReviewState {
-  ReviewLoadingState();
-}
+//
+// class ReviewCompleteState extends ReviewState {
+//   ReviewCompleteState.FlashCardCompleteState(lessonEntity) {
+//     this.lessonEntity = lessonEntity;
+//   }
+// }
+//
 
-class ReviewCompleteState extends ReviewState {
-  ReviewCompleteState.FlashCardCompleteState(lessonEntity) {
-    this.lessonEntity = lessonEntity;
-  }
-}
-
-class ReviewErrorState extends ReviewState {
-  String? msgError;
-  int? status;
-
-  ReviewErrorState(this.msgError, this.status);
-}
-
-class ReviewNextState extends ReviewState {}
-
-class ReviewPreviousState extends ReviewState {}
-
-class ReviewAgainState extends ReviewState {}
+//   ReviewErrorState(this.msgError, this.status);
+// }
+//
+// class ReviewNextState extends ReviewState {}
+//
+// class ReviewPreviousState extends ReviewState {}
+//
+// class ReviewAgainState extends ReviewState {}
